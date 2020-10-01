46537bec96c7ad73fda492a9be73a35f715de29d
AllenJB <github@allenjb.me.uk>
2020-10-01 13:41:27 +0100

    Update SQL schema & README

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Google Play Music Playlist Tracker
+# PlayList Tracker
+
+## Google Play Music
 
 Set of scripts for monitoring and managing my GPM playlists.
 
@@ -9,6 +11,15 @@ Additionally I have collections that span several playlists due to their size
 and the GPM playlist track limit (1000 tracks). `gpm-rpg.py` creates a playlist
 made up of a random selection from this collection. `gpm-rp-cascade.py`
 cascades deletions from the randomly generated lists back to the source lists.
+
+## YouTube Music
+
+Monitors playlists for removed / unavailable tracks.
+
+The web interface provides the facility to search for tracks in your playlists and remove the selected track from one
+or all playlists.
+
+Since YTM provides for larger playlists, the scripts for generating a random selection playlist have not been migrated.
 
 ## Requirements
 
@@ -26,7 +37,13 @@ pipenv install
 pipenv run python3 gpm-auth.py
 ```
 
-## Example crontab
+### Google Play Music
+
+```bash
+pipenv run python3 gpm-auth.py
+```
+
+#### Example crontab
 
 ```
 # mn hr  dom mon dow  cmd
@@ -35,4 +52,24 @@ pipenv run python3 gpm-auth.py
 15 4 * * *  cd ${HOME}/projects/plt/; pipenv run python3 gpm-rp-cascade.py
 # Run fetch again to record the new playlist and cascaded changes
 30 4 * * *  cd ${HOME}/projects/plt/; pipenv run python3 gpm-fetch.py
+```
+
+### YouTube Music
+
+Unfortunately the official YouTube API does not work well with YouTube Music content. Uploaded (and many GPM migrated)
+tracks always appear as private without details even when fully authenticated. This means we have to use an "unofficial"
+API based on the web interface, which currently does not provide proper OAuth authentication.
+
+To set up authentication follow the [ytmusicapi setup instructions](https://ytmusicapi.readthedocs.io/en/latest/setup.html)
+to obtain the authentication headers and paste them into the setup script when prompted:
+
+```bash
+pipenv run python3 -m ytm.setup
+```
+
+#### Example crontab
+
+```bash
+# mn hr  dom mon dow  cmd
+@daily                cd ${HOME}/projects/plt; pipenv run python3 -m ytm.fetch 
 ```
--- schema.sql
+++ schema.sql
@@ -52,6 +52,7 @@ CREATE TABLE IF NOT EXISTS `gpm_playlists` (
   `gpm_playlistid` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `dt_created` datetime NOT NULL,
   `dt_modified` datetime DEFAULT NULL,
+  `dt_deleted` datetime DEFAULT NULL,
   `deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
   `dt_recent` datetime DEFAULT NULL,
   `access_controlled` tinyint(1) unsigned DEFAULT NULL,
@@ -61,6 +62,7 @@ CREATE TABLE IF NOT EXISTS `gpm_playlists` (
   `owner_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `share_token` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `type` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `processed` tinyint(1) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`playlistid`),
   UNIQUE KEY `gpm_playlistid` (`gpm_playlistid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -122,6 +124,95 @@ CREATE TABLE IF NOT EXISTS `gpm_track_artists` (
   UNIQUE KEY `trackid_albumid_artistid` (`trackid`,`albumid`,`artistid`),
   KEY `artistid` (`artistid`),
   KEY `albumid` (`albumid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `ytm_albums` (
+  `albumid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ytm_browseid` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `album_name` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`albumid`),
+  UNIQUE KEY `ytm_browseid` (`ytm_browseid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `ytm_artists` (
+  `artistid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ytm_channelid` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `artist_name` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`artistid`) USING BTREE,
+  UNIQUE KEY `ytm_channelid` (`ytm_channelid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `ytm_playlists` (
+  `playlistid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `dt_plt_created` datetime NOT NULL,
+  `dt_plt_deleted` datetime DEFAULT NULL,
+  `deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `ytm_playlistid` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `playlist_title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `track_count` int(10) unsigned NOT NULL,
+  `processed` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`playlistid`),
+  UNIQUE KEY `ytm_playlistid` (`ytm_playlistid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `ytm_playlist_entries` (
+  `entryid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ytm_playlistid` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ytm_set_videoid` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ytm_videoid` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `dt_plt_created` datetime NOT NULL,
+  `dt_plt_deleted` datetime DEFAULT NULL,
+  `deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `artist_name` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `album_name` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `entry_name` varchar(512) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `unavailable` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `duration` time DEFAULT NULL,
+  `prev_unavailable` tinyint(1) unsigned DEFAULT NULL,
+  `processed` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`entryid`),
+  UNIQUE KEY `ytm_playlistid_ytm_set_videoid` (`ytm_playlistid`,`ytm_set_videoid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `ytm_playlist_entries_unavailable` (
+  `uentryid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `dt_plt_created` datetime NOT NULL,
+  `dt_plt_deleted` datetime DEFAULT NULL,
+  `deleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `ytm_playlistid` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `artist_name` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `album_name` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `entry_name` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `duration` time DEFAULT NULL,
+  `processed` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`uentryid`),
+  UNIQUE KEY `ytm_playlist_id_artist_name_entry_title` (`ytm_playlistid`,`artist_name`,`entry_name`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `ytm_videos` (
+  `vid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ytm_videoid` varchar(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `video_title` varchar(512) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `duration` time DEFAULT NULL,
+  `like_status` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`vid`),
+  UNIQUE KEY `ytm_videoid` (`ytm_videoid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `ytm_video_albums` (
+  `linkid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `videoid` int(10) unsigned NOT NULL,
+  `albumid` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`linkid`),
+  UNIQUE KEY `videoid_albumid` (`videoid`,`albumid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `ytm_video_artists` (
+  `linkid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `videoid` int(10) unsigned NOT NULL,
+  `artistid` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`linkid`),
+  UNIQUE KEY `videoid_artistid` (`videoid`,`artistid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
